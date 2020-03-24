5e21e8e94b91bb4f10a9d0ee9761e539fc91e6e4
u2waremanager@gmail.com <u2waremanager@gmail.com>
2020-03-24 15:01:50 +0900

    2.1.5.2.RELEASE

--- src/main/java/org/springframework/data/jpa/repository/query/support/AbstractWhereBuilder.java
+++ src/main/java/org/springframework/data/jpa/repository/query/support/AbstractWhereBuilder.java
@@ -223,12 +223,12 @@ public abstract class AbstractWhereBuilder<X> {
 		}
 
 		public W like(String property, String right) {
-			if(StringUtils.hasText(right)) return where;
+			if(StringUtils.isEmpty(right)) return where;
 			return add(builder.getPath().getString(property).like(right));
 		}
 		
 		public W notLike(String property, String right) {
-			if(StringUtils.hasText(right)) return where;
+			if(StringUtils.isEmpty(right)) return where;
 			return add(builder.getPath().getString(property).notLike(right));
 		}
 
